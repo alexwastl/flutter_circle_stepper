dc8e96bdcaa80aaa336f2fd335e487332dda8be4
Alexander Wastl <a.wastl@germanbionic.com>
2024-09-06 11:27:40 +0200

    feat: init stepper project

---
+++ lib/stepper_navigation.dart
@@ -0,0 +1,12 @@
+library stepper_navigation;
+
+import 'package:flutter/widgets.dart';
+
+class StepperNavigation extends StatelessWidget {
+  const StepperNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
